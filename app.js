e338f3bea196a696142a583618970fa7939c01f3
pokhareldiwash2000 <48590209+pokhareldiwash2000@users.noreply.github.com>
2023-05-27 09:18:15 +0545

    Update app.js auth token header

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use('/api/filter',filterRoute);
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE ,PATCH');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, auth-token');
   next();
 });
 
