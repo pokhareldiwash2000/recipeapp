14f73b2598597ed60ad2e50dc7e3195feb6c212e
pokhareldiwash2000 <48590209+pokhareldiwash2000@users.noreply.github.com>
2023-05-27 08:57:11 +0545

    Update app.js

--- app.js
+++ app.js
@@ -35,8 +35,8 @@ app.use('/api/filter',filterRoute);
 
 // Enable CORS for all routes
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://yourfrontenddomain.com');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Origin', 'https://recipeapp-lh51.onrender.com');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE ,PATCH');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
