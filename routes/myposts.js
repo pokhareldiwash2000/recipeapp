99fda0e9fd32af40997ffc9c5637500593fca8ca
Diwash pokharel <pokhareldiwash2000@gmail.com>
2023-06-01 10:47:46 +0545

    steps,ingredients changed to Json.stringify() capable

--- routes/myposts.js
+++ routes/myposts.js
@@ -38,14 +38,16 @@ router.post('/',
   const { name, description, servings, cookingTime, prepTime} = req.body;
   const author=req.user._id;
   const photo = req.files['photo'] ? `/media/${req.files['photo'][0].filename}` : '';
-  const steps = [];
+  const steps=[];
+  //if sent as an array of array
   // if(req.body.ingredients){
   //   const ingred=req.body.ingredients.map(([name, quantity]) => ({ name, quantity }));
   // req.body.ingredients=ingred;
   // }
   // if(req.body.steps){const stp=req.body.steps.map(name => ({ name }));
   // req.body.steps=stp;}
-  const ingredients=req.body.ingredients;
+  
+  const ingredients=req.body.ingredients?JSON.parse(req.body.ingredients):[];
   const cuisinecategory= req.body.cuisinecategory?req.body.cuisinecategory:[];
   const coursecategory= req.body.coursecategory?req.body.coursecategory:[];
   const dietcategory= req.body.dietcategory?req.body.dietcategory:[];
@@ -53,7 +55,7 @@ router.post('/',
   // req.body.steps=JSON.parse(req.body.steps);
   
   // const ingredients=JSON.parse(req.body.ingredients)
-
+  if(req.body.steps) req.body.steps=JSON.parse(req.body.steps);
   if (req.body.steps && req.body.steps.length) {
     for (let i = 0; i < req.body.steps.length; i++) {
       const step = { name: req.body.steps[i].name };
@@ -75,72 +77,7 @@ router.post('/',
     res.status(500).json({ error: err });
   }
 });
-// // Create a new post
-// router.post(
-//   '/',
-//   verify,
-//   uploadPhoto.fields([
-//     { name: 'photo', maxCount: 1 },
-//     { name: 'stepsPhoto', maxCount: 10 },
-//     { name: 'video', maxCount: 1 },
-//   ]),
-//   async (req, res, next) => {
-//     res.send(req.body);
-//     try {
-//       const { name, description, servings, cookingTime, prepTime } = req.body;
-//       const author = req.user._id;
-//       const photo = req.files['photo'] ? `/media/${req.files['photo'][0].filename}` : '';
-//       const steps = [];
-//       const ingredients = req.body.ingredients;
-//       const cuisinecategory = req.body.cuisinecategory ? req.body.cuisinecategory : [];
-//       const coursecategory = req.body.coursecategory ? req.body.coursecategory : [];
-//       const dietcategory = req.body.dietcategory ? req.body.dietcategory : [];
-//       const youtubeURL = req.body.youtubeURL || '';
-//       const video = req.files['video'] ? `/media/${req.files['video'][0].filename}` : '';
 
-//       if (req.body.steps && req.body.steps.length) {
-//         for (let i = 0; i < req.body.steps.length; i++) {
-//           const step = { name: req.body.steps[i].name };
-//           if (req.files['stepsPhoto'] && req.files['stepsPhoto'][i]) {
-//             step.photo = `/media/${req.files['stepsPhoto'][i].filename}`;
-//           }
-//           steps.push(step);
-//         }
-//       }
-
-//       const post = new Post({
-//         author,
-//         name,
-//         description,
-//         photo,
-//         youtubeURL,
-//         servings,
-//         cookingTime,
-//         prepTime,
-//         ingredients,
-//         steps,
-//         cuisinecategory,
-//         coursecategory,
-//         dietcategory,
-//         video,
-//       });
-
-//       // Set a timeout of 10 seconds
-//       const timeout = setTimeout(() => {
-//         throw new Error('Request timed out');
-//       }, 10000);
-
-//       const result = await post.save();
-
-//       clearTimeout(timeout); // Clear the timeout since the request completed successfully
-
-//       res.status(201).json({ message: 'Post created successfully.', post: result });
-//     } catch (err) {
-//       console.error(err);
-//       res.status(500).json({ error: 'An error occurred while processing the request.' });
-//     }
-//   }
-// );
 
 // Update an existing post
 router.patch('/:id',
@@ -162,7 +99,7 @@ router.patch('/:id',
     // }
     // if(req.body.steps){const stp=req.body.steps.map(name => ({ name }));
     // req.body.steps=stp;}
-    const ingredients=req.body.ingredients?req.body.ingredients:p.ingredients;
+    const ingredients=req.body.ingredients?JSON.parse(req.body.ingredients):p.ingredients;
     const cuisinecategory= req.body.cuisinecategory?req.body.cuisinecategory:p.cuisinecategory;
     const coursecategory= req.body.coursecategory?req.body.coursecategory:p.coursecategory;
     const dietcategory= req.body.dietcategory?req.body.dietcategory:p.dietcategory;
@@ -190,6 +127,7 @@ router.patch('/:id',
       photo = `/media/${req.files['photo'][0].filename}`;
     }
     let steps = p.steps;
+    if(req.body.steps) req.body.steps=JSON.parse(req.body.steps);
     if (req.body.steps) {
       steps = [];
       // req.body.steps = JSON.parse(req.body.steps);
